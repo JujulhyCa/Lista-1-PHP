0ed17c07642b831b9cdb7421d67823d94e49e3fa
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-17 23:11:18 -0300

    Exercício 6
    6. Faça um algoritmo PHP que receba os valores A e B, imprima-os em ordem crescente em relação aos
    seus valores. Caso os valores sejam iguais, informar o usuário e imprimir o valor em tela apenas uma
    vez.
    Exemplo, para A=5, B=4 você deve imprimir na tela: "4 5".
     para A=5, B=5 você deve imprimir na tela: “Números iguais: 5”

---
+++ Exerc6.php
@@ -0,0 +1,17 @@
+<!-- 6. Faça um algoritmo PHP que receba os valores A e B, imprima-os em ordem crescente em relação aos 
+seus valores. Caso os valores sejam iguais, informar o usuário e imprimir o valor em tela apenas uma 
+vez.
+Exemplo, para A=5, B=4 você deve imprimir na tela: "4 5".
+ para A=5, B=5 você deve imprimir na tela: “Números iguais: 5”-->
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    
+ </body>
+ </html>
