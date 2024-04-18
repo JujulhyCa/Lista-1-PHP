70757a43c337fb784981389146d07a5f38ba34fe
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-17 22:10:37 -0300

    Exercício 1
    Enunciado

--- Exerc1.php
+++ Exerc1.php
@@ -1,3 +1,6 @@
+<!-- 1. Crie um algoritmo que receba um número digitado pelo usuário e verifique se esse valor é positivo, 
+negativo ou igual a zero. A saída deve ser: "Valor Positivo", "Valor Negativo" ou "Igual a Zero". -->
+
 <!DOCTYPE html>
 <html lang="pt-Br">
 <head>
