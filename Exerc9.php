91048d2b180c318f7fdd80a029a5980762aff281
JulhyCar <julhy.carvalho@users.noreply.github.com>
2024-04-18 18:10:08 -0300

    Create Exerc9.php
    9. Faça um script PHP que receba de um formulário HTML5 uma variável com o ano de nascimento de uma pessoa, crie uma constante com o ano atual, calcule e mostre:
    a. a idade dessa pessoa em anos;
    b. quantos dias esta pessoa já viveu;
    c. quantos anos essa pessoa terá em 2025

---
+++ Exerc9.php
@@ -0,0 +1,17 @@
+<!-- 9. Faça um script PHP que receba de um formulário HTML5 uma variável com o ano de nascimento de
+uma pessoa, crie uma constante com o ano atual, calcule e mostre:
+a. a idade dessa pessoa em anos;
+b. quantos dias esta pessoa já viveu;
+c. quantos anos essa pessoa terá em 2025-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
